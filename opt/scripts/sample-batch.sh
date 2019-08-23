55940a25036a717ae4c1da490bf753369444d02a
yongyct <53852395+yongyct@users.noreply.github.com>
2019-08-23 16:11:44 +0800

    Update sample-batch.sh

--- opt/scripts/sample-batch.sh
+++ opt/scripts/sample-batch.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/sh
 
+hostname=$(hostname)
+dt=$(date +"%Y%m%d_%H%M%S")
+pids=( $(pgrep -f "mycommand myargs") )
+
+if [[ -z "$pids" ]]
+then
+  if [[ $hostname = "devhostpattern"* ]]
+  then
+    mycommand mydevargs
+  elif [[ $hostname = "uathostpattern"* ]]
+  then
+    mycommand myuatargs
+  elif [[ $hostname = "prdhostpattern"* ]]
+  then
+    mycommand myprdargs
+  fi
+else
+  echo "Another program is currently running, ending this program"
+fi
