d0698579730ee8d48d0f33b52872916fd8f990a8
yongyct <53852395+yongyct@users.noreply.github.com>
2019-09-04 14:42:40 +0800

    Create mp_sample.py

---
+++ opt/python/mp_sample.py
@@ -0,0 +1,24 @@
+'''
+Sample template for multiprocessing
+'''
+import multiprocessing as mp
+
+def my_func(my_args):
+    print(my_args)
+
+# Set number of jobs based on percentage of cpu available
+n_jobs = mp.cpu_count // 2
+pool = mp.Pool(n_jobs)
+
+# E.g. iterable batch of data to be processed
+all_my_data = [i for i in range(1000)]
+
+try:
+    tasks = [pool.apply_async(my_func, (my_data, )) for my_data in all_my_data]
+    for task in tasks:
+        task.get()
+    pool.join()
+    pool.close()
+except KeyboardInterrupt:
+    pool.close()
+    pool.terminate()
