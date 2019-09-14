84b1ae76bb0e3f218ea8fe23254a325f754790e4
yongyct <53852395+yongyct@users.noreply.github.com>
2019-09-14 11:27:01 +0800

    Update client.sh

--- opt/kafka/client.sh
+++ opt/kafka/client.sh
@@ -1,5 +1,7 @@
 !#/usr/bin/sh
 
+# Default zk port = 2181, broker port = 9092
+
 # KAFKA_OPTS to be set
 ## Consumer
 /path/to/kafka-console-consumer --consumer.config /path/to/client.properties --topic mytopic --bootstrap-server kafkahost1:port1,kafkahost2:port2 --property print.timestamp=true
@@ -10,3 +12,6 @@
 # SCHEMA_REGISTRY_OPTS to be set
 ## Avro Consumer
 /path/to/kafka-avro-console-consumer --consumer.config /path/to/client.properties --topic mytopic --bootstrap-server kafkahost1:port1,kafkahost2:port2 --property print.timestamp=true --property schema.registry.url=host:port --from-beginning
+
+## Create topics
+./kafka-topics --create --zookeeper zkhost:zkport --partitions 1 --replication-factor 1 --config retention.ms=240000 --topic mytopic
